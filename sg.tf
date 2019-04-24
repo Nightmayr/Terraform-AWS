3fc805338e0df896ca0ef49709aa6ceef1b63479
Nightmayr <umayr23@gmail.com>
2019-04-24 18:25:27 +0100

    http and https ingress rules added

--- sg.tf
+++ sg.tf
@@ -8,10 +8,42 @@ resource "aws_security_group" "allow_ssh" {
     to_port   = 22
     protocol  = "tcp"
 
-    #cidr_blocks = # add your IP address here
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
     Name = "allow_ssh"
+  }
+}
+
+resource "aws_security_group" "allow_https" {
+  name        = "allow_https"
+  description = "Allow HTTPS"
+  vpc_id      = "${aws_vpc.main.id}"
+
+  ingress {
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+  }
+
+  tags = {
+    Name = "allow_https"
+  }
+}
+
+resource "aws_security_group" "allow_http" {
+  name        = "allow_http"
+  description = "Allow HTTP"
+  vpc_id      = "${aws_vpc.main.id}"
+
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+  }
+
+  tags = {
+    Name = "allow_http"
   }
 }
