db1711da9039d6ce811d47bcefa76bf6435b9d0c
Nightmayr <umayr23@gmail.com>
2019-04-25 18:05:14 +0100

    routing table defined and attached to igw. routing table associated with subnet-1

--- subnet.tf
+++ subnet.tf
@@ -29,3 +29,21 @@ resource "aws_subnet" "subnet-3" {
     Name = "Subnet-3"
   }
 }
+
+resource "aws_route_table" "route-table-main-vpc" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.main-vpc-igw.id}"
+  }
+
+  tags {
+    Name = "route-table-main-vpc"
+  }
+}
+
+resource "aws_route_table_association" "subnet-association" {
+  subnet_id      = "${aws_subnet.subnet-1.id}"
+  route_table_id = "${aws_route_table.route-table-main-vpc.id}"
+}
