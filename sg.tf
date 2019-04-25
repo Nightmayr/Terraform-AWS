edf932bd261136495d0bf06f8300dfe581edb1fb
Nightmayr <umayr23@gmail.com>
2019-04-25 17:17:22 +0100

    added egress within ssh rule

--- sg.tf
+++ sg.tf
@@ -4,11 +4,18 @@ resource "aws_security_group" "allow_ssh" {
   vpc_id      = "${aws_vpc.main.id}"
 
   ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
-
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # allow all outgoing
+  egress {
+    from_port  = 0
+    to_port    = 0
+    protocol   = "-1"
+    cidr_block = ["0.0.0.0/0"]
   }
 
   tags = {
