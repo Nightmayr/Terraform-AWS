d6ed7ec2ef8883daa09686fe7d74d04f81579ef7
Nightmayr <umayr23@gmail.com>
2019-04-25 17:26:37 +0100

    fixed typo

--- sg.tf
+++ sg.tf
@@ -12,10 +12,10 @@ resource "aws_security_group" "allow_ssh" {
 
   # allow all outgoing
   egress {
-    from_port  = 0
-    to_port    = 0
-    protocol   = "-1"
-    cidr_block = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
